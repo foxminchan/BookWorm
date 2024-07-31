b0a2605f11ab77e4cc960122352d5fa44b47709d
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-07-31 20:20:05 +0700

    chore: Remove unused code files, update database context and repository names in BookWorm.Ordering

--- src/BookWorm.AppHost/Program.cs
+++ src/BookWorm.AppHost/Program.cs
@@ -112,26 +112,6 @@ var notificationApi = builder.AddProject<BookWorm_Notification>("notification-ap
     .WaitFor(notificationDb)
     .WaitFor(smtpServer);
 
-// Reverse proxy
-var bff = builder.AddProject<BookWorm_Web_Bff>("bff")
-    .WithExternalHttpEndpoints()
-    .WithEnvironment("BFF__Authority", identityEndpoint)
-    .WithEnvironment(ResourceTemplate("catalog"), catalogApi.GetEndpoint(launchProfileName))
-    .WithEnvironment(ResourceTemplate("ordering"), orderingApi.GetEndpoint(launchProfileName))
-    .WithEnvironment(ResourceTemplate("rating"), ratingApi.GetEndpoint(launchProfileName))
-    .WithEnvironment(ResourceTemplate("basket"), basketApi.GetEndpoint(launchProfileName));
-
-// Web
-var storeFront = builder.AddProject<BookWorm_StoreFront>("storefront")
-    .WithExternalHttpEndpoints()
-    .WithEnvironment("BFF__Url", bff.GetEndpoint(launchProfileName));
-
-var backOffice = builder
-    .AddNpmApp("backoffice", "../BookWorm.Backoffice", "dev")
-    .WithHttpEndpoint(3000, env: "PORT")
-    .WithEnvironment("BROWSER", "none")
-    .PublishAsDockerFile();
-
 // Health checks
 builder.AddHealthChecksUi("healthchecksui")
     .WithReference(identityApi)
@@ -140,22 +120,21 @@ builder.AddHealthChecksUi("healthchecksui")
     .WithReference(ratingApi)
     .WithReference(basketApi)
     .WithReference(notificationApi)
-    .WithReference(storeFront)
-    .WithReference(backOffice)
-    .WithReference(bff)
     .WithExternalHttpEndpoints();
 
 identityApi
-    .WithEnvironment("Services__Bff", bff.GetEndpoint(launchProfileName))
-    .WithEnvironment("Services__StoreFront", storeFront.GetEndpoint(launchProfileName))
-    .WithEnvironment("Services__BackOffice", backOffice.GetEndpoint("http"))
     .WithEnvironment("Services__Catalog", catalogApi.GetEndpoint(launchProfileName))
     .WithEnvironment("Services__Ordering", orderingApi.GetEndpoint(launchProfileName))
     .WithEnvironment("Services__Rating", ratingApi.GetEndpoint(launchProfileName))
     .WithEnvironment("Services__Basket", basketApi.GetEndpoint(launchProfileName));
 
-builder.Build().Run();
-return;
+builder.AddYarp("ingress")
+    .WithEndpoint(scheme: launchProfileName, port: 80)
+    .WithReference(identityApi)
+    .WithReference(catalogApi)
+    .WithReference(orderingApi)
+    .WithReference(ratingApi)
+    .WithReference(basketApi)
+    .LoadFromConfiguration("ReverseProxy");
 
-static string ResourceTemplate(string service) =>
-    $"ReverseProxy__Clusters__{service}__Destinations__{service}__Address";
+builder.Build().Run();
