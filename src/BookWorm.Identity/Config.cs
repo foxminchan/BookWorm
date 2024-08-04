b330e8c9f407f545c475d047765dc49018f3964c
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-08-04 17:14:14 +0700

    chore: Update stylesheet and appsettings.json, add project references, and configure Redis data protection

--- src/BookWorm.Identity/Config.cs
+++ src/BookWorm.Identity/Config.cs
@@ -1,4 +1,5 @@
 ﻿using BookWorm.Identity.Options;
+using Duende.IdentityServer;
 using Duende.IdentityServer.Models;
 
 namespace BookWorm.Identity;
@@ -79,6 +80,31 @@ public static class Config
                 RedirectUris = { $"{service.Rating}/swagger/rating-api/oauth2-redirect.html" },
                 PostLogoutRedirectUris = { $"{service.Rating}/swagger/" },
                 AllowedScopes = { "rating" }
+            },
+            new()
+            {
+                ClientId = "bff",
+                ClientName = "Backend For Frontend",
+                ClientSecrets = { new("secret".Sha256()) },
+                AllowedGrantTypes = [GrantType.AuthorizationCode],
+                RedirectUris = { $"{service.Bff}/signin-oidc" },
+                BackChannelLogoutUri = $"{service.Bff}/bff/backchannel",
+                PostLogoutRedirectUris = { $"{service.Bff}/signout-callback-oidc" },
+                AllowedCorsOrigins = { $"{service.Bff}" },
+                AllowOfflineAccess = true,
+                AlwaysIncludeUserClaimsInIdToken = true,
+                AllowedScopes =
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    IdentityServerConstants.StandardScopes.OfflineAccess,
+                    "catalog",
+                    "ordering",
+                    "basket",
+                    "rating"
+                },
+                AccessTokenLifetime = 60 * 60 * 2,
+                IdentityTokenLifetime = 60 * 60 * 2
             }
         ];
     }
