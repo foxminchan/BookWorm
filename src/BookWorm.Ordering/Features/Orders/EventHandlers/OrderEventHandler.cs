51b3cd793fd7cc66482ee19a1a4a12eb16cfeae1
Nguyen Xuan Nhan <nguyenxuannhan407@gmail.com>
2024-08-15 17:09:25 +0700

    chore: Add order event sourcing model

---
+++ src/BookWorm.Ordering/Features/Orders/EventHandlers/OrderEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using BookWorm.Ordering.Domain.OrderAggregate;
+using BookWorm.Ordering.Domain.OrderAggregate.Events;
+using Marten;
+using MediatR;
+
+namespace BookWorm.Ordering.Features.Orders.EventHandlers;
+
+public sealed class OrderEventHandler(IDocumentSession documentSession) : INotificationHandler<OrderCreatedEvent>,
+    INotificationHandler<OrderCompletedEvent>, INotificationHandler<OrderCancelledEvent>
+{
+    public async Task Handle(OrderCancelledEvent notification, CancellationToken cancellationToken)
+    {
+        await documentSession.Events.WriteToAggregate<OrderState>(Guid.NewGuid(),
+            stream => { stream.AppendOne(notification); }, cancellationToken);
+    }
+
+    public async Task Handle(OrderCompletedEvent notification, CancellationToken cancellationToken)
+    {
+        await documentSession.Events.WriteToAggregate<OrderState>(Guid.NewGuid(),
+            stream => { stream.AppendOne(notification); }, cancellationToken);
+    }
+
+    public async Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        await documentSession.Events.WriteToAggregate<OrderState>(Guid.NewGuid(),
+            stream => { stream.AppendOne(notification); }, cancellationToken);
+    }
+}
