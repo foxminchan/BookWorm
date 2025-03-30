e71dddbed954fc99b2550550e9acefdafc4e0a99
Nhan Nguyen <nguyenxuannhan407@gmail.com>
2025-03-30 18:06:22 +0700

    docs: enhance documentations

--- src/Services/Ordering/BookWorm.Ordering.UnitTests/Features/Orders/Cancel/CancelOrderEndpointTests.cs
+++ src/Services/Ordering/BookWorm.Ordering.UnitTests/Features/Orders/Cancel/CancelOrderEndpointTests.cs
@@ -1,4 +1,5 @@
-﻿using BookWorm.Ordering.Features.Orders;
+﻿using BookWorm.Ordering.Domain.AggregatesModel.OrderAggregate;
+using BookWorm.Ordering.Features.Orders;
 using BookWorm.Ordering.Features.Orders.Cancel;
 using BookWorm.SharedKernel.Exceptions;
 using MediatR;
@@ -20,7 +21,13 @@ public sealed class CancelOrderEndpointTests
         _orderId = Guid.CreateVersion7();
 
         // Create a sample OrderDetailDto to return from the sender
-        _orderDetailDto = new(_orderId, DateTime.UtcNow, 100.0m, new List<OrderItemDto>());
+        _orderDetailDto = new(
+            _orderId,
+            DateTime.UtcNow,
+            100.0m,
+            Status.Cancelled,
+            new List<OrderItemDto>()
+        );
     }
 
     [Test]
