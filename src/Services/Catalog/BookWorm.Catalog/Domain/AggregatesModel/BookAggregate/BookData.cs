586e51dbe5c72c3e10be1ff632dd1618a2b10d45
Nhan Nguyen <nguyenxuannhan407@gmail.com>
2025-04-12 12:06:43 +0700

    refactor: reduce duplication rate and improve test coverage rate

--- src/Services/Catalog/BookWorm.Catalog/Domain/AggregatesModel/BookAggregate/BookData.cs
+++ src/Services/Catalog/BookWorm.Catalog/Domain/AggregatesModel/BookAggregate/BookData.cs
@@ -5,175 +5,39 @@ public sealed class BookData : List<Book>
 {
     public BookData()
     {
-        const string fakeDescription = "Description";
-
         AddRange(
             [
-                new(
-                    "The Great Gatsby",
-                    fakeDescription,
-                    null,
-                    10.99m,
-                    5.2m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "To Kill a Mockingbird",
-                    fakeDescription,
-                    null,
-                    7.99m,
-                    4.5m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new("1984", fakeDescription, null, 8.99m, 4.8m, Guid.Empty, Guid.Empty, []),
-                new(
-                    "Pride and Prejudice",
-                    fakeDescription,
-                    null,
-                    6.99m,
-                    4.2m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "The Catcher in the Rye",
-                    fakeDescription,
-                    null,
-                    9.99m,
-                    4.0m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new("The Hobbit", fakeDescription, null, 12.99m, 6.5m, Guid.Empty, Guid.Empty, []),
-                new(
-                    "The Lord of the Rings",
-                    fakeDescription,
-                    null,
-                    20.99m,
-                    10.0m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "The Da Vinci Code",
-                    fakeDescription,
-                    null,
-                    14.99m,
-                    7.0m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "The Alchemist",
-                    fakeDescription,
-                    null,
-                    11.99m,
-                    5.8m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "The Hunger Games",
-                    fakeDescription,
-                    null,
-                    13.99m,
-                    6.2m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new("The Fault in Our Stars", null, null, 9.99m, 4.5m, Guid.Empty, Guid.Empty, []),
-                new(
-                    "The Book Thief",
-                    fakeDescription,
-                    null,
-                    10.99m,
-                    5.0m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "The Kite Runner",
-                    fakeDescription,
-                    null,
-                    8.99m,
-                    4.7m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "The Fault in Our Stars",
-                    fakeDescription,
-                    null,
-                    9.99m,
-                    4.5m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "The Great Alone",
-                    fakeDescription,
-                    null,
-                    11.99m,
-                    5.2m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "The Night Circus",
-                    fakeDescription,
-                    null,
-                    13.99m,
-                    6.5m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new("Circe", fakeDescription, null, 14.99m, 7.0m, Guid.Empty, Guid.Empty, []),
-                new(
-                    "The Silent Patient",
-                    fakeDescription,
-                    null,
-                    15.99m,
-                    7.5m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "Anxious People",
-                    fakeDescription,
-                    null,
-                    17.99m,
-                    8.5m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
-                new(
-                    "Atomic Habits",
-                    fakeDescription,
-                    null,
-                    18.99m,
-                    9.0m,
-                    Guid.Empty,
-                    Guid.Empty,
-                    []
-                ),
+                CreateBook("The Great Gatsby", 10.99m, 5.2m),
+                CreateBook("To Kill a Mockingbird", 7.99m, 4.5m),
+                CreateBook("1984", 8.99m, 4.8m),
+                CreateBook("Pride and Prejudice", 6.99m, 4.2m),
+                CreateBook("The Catcher in the Rye", 9.99m, 4.0m),
+                CreateBook("The Hobbit", 12.99m, 6.5m),
+                CreateBook("The Lord of the Rings", 20.99m, 10.0m),
+                CreateBook("The Da Vinci Code", 14.99m, 7.0m),
+                CreateBook("The Alchemist", 11.99m, 5.8m),
+                CreateBook("The Hunger Games", 13.99m, 6.2m),
+                CreateBook("The Fault in Our Stars", 9.99m, 4.5m),
+                CreateBook("The Book Thief", 10.99m, 5.0m),
+                CreateBook("The Kite Runner", 8.99m, 4.7m),
+                CreateBook("The Fault in Our Stars", 9.99m, 4.5m),
+                CreateBook("The Great Alone", 11.99m, 5.2m),
+                CreateBook("The Night Circus", 13.99m, 6.5m),
+                CreateBook("Circe", 14.99m, 7.0m),
+                CreateBook("The Silent Patient", 15.99m, 7.5m),
+                CreateBook("Anxious People", 17.99m, 8.5m),
+                CreateBook("Atomic Habits", 18.99m, 9.0m),
             ]
         );
+    }
+
+    private static Book CreateBook(
+        string name,
+        decimal price,
+        decimal? priceSale,
+        string? description = null
+    )
+    {
+        return new(name, description, null, price, priceSale, Guid.Empty, Guid.Empty, []);
     }
 }
