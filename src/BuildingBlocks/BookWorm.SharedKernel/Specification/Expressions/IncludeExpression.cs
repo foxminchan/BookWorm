650a89f6388afc0c424b67196352d1f107867623
Nhan Nguyen <nguyenxuannhan407@gmail.com>
2025-05-03 22:49:15 +0700

    fix: Include/ThenInclude support for entity relationships in specifications

--- src/BuildingBlocks/BookWorm.SharedKernel/Specification/Expressions/IncludeExpression.cs
+++ src/BuildingBlocks/BookWorm.SharedKernel/Specification/Expressions/IncludeExpression.cs
@@ -4,15 +4,28 @@ namespace BookWorm.SharedKernel.Specification.Expressions;
 
 public class IncludeExpression
 {
-    public IncludeExpression(LambdaExpression expression, IncludeType includeType)
+    public IncludeExpression(LambdaExpression expression)
     {
         _ = expression ?? throw new ArgumentNullException(nameof(expression));
 
         LambdaExpression = expression;
-        Type = includeType;
+        PreviousPropertyType = null;
+        Type = IncludeType.Include;
+    }
+
+    public IncludeExpression(LambdaExpression expression, Type previousPropertyType)
+    {
+        _ = expression ?? throw new ArgumentNullException(nameof(expression));
+        _ = previousPropertyType ?? throw new ArgumentNullException(nameof(previousPropertyType));
+
+        LambdaExpression = expression;
+        PreviousPropertyType = previousPropertyType;
+        Type = IncludeType.ThenInclude;
     }
 
     public LambdaExpression LambdaExpression { get; }
+
+    public Type? PreviousPropertyType { get; }
 
     public IncludeType Type { get; }
 }
