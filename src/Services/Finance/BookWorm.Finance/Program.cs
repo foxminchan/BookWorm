8ea121dfd75eef05aee88487c0f3bde234e416da
Nhan Nguyen <nguyenxuannhan407@gmail.com>
2025-07-28 12:02:28 +0700

    refactor: update package versions, remove deprecated endpoints, and clean up unused code

--- src/Services/Finance/BookWorm.Finance/Program.cs
+++ src/Services/Finance/BookWorm.Finance/Program.cs
@@ -19,20 +19,6 @@ app.UseStatusCodePages();
 
 app.UseOutputCache();
 
-app.UseDefaultCors();
-
-app.UseRequestTimeouts();
-
-app.UseMiddleware<KeycloakTokenIntrospectionMiddleware>();
-
-app.UseRateLimiter();
-
-var apiVersionSet = app.NewApiVersionSet().HasApiVersion(new(1, 0)).ReportApiVersions().Build();
-
-app.MapEndpoints(apiVersionSet);
-
 app.MapDefaultEndpoints();
-
-app.UseDefaultOpenApi();
 
 app.Run();
