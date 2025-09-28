1109fc0fdae136fba0a809a72b7e3a4f3f7f2568
Nhan Nguyen <nguyenxuannhan407@gmail.com>
2025-09-28 12:23:48 +0700

    fix: update package versions and add descriptions to enums

--- src/BuildingBlocks/BookWorm.Chassis/Specification/IncludeType.cs
+++ src/BuildingBlocks/BookWorm.Chassis/Specification/IncludeType.cs
@@ -1,7 +1,12 @@
-﻿namespace BookWorm.Chassis.Specification;
+﻿using System.ComponentModel;
+
+namespace BookWorm.Chassis.Specification;
 
 public enum IncludeType : byte
 {
+    [Description("Include")]
     Include = 0,
+
+    [Description("Then Include")]
     ThenInclude = 1,
 }
