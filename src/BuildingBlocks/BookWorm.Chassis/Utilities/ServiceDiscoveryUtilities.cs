d86f1faf5a7d5bd5631f0735171633606d3ef483
Nhan Nguyen <nguyenxuannhan407@gmail.com>
2025-10-06 21:14:50 +0700

    refactor: Pre-define OAuth 2.x for A2A

--- src/BuildingBlocks/BookWorm.Chassis/Utilities/ServiceDiscoveryUtilities.cs
+++ src/BuildingBlocks/BookWorm.Chassis/Utilities/ServiceDiscoveryUtilities.cs
@@ -1,4 +1,6 @@
-﻿namespace BookWorm.Chassis.Utilities;
+﻿using BookWorm.Constants.Aspire;
+
+namespace BookWorm.Chassis.Utilities;
 
 public static class ServiceDiscoveryUtilities
 {
@@ -7,5 +9,15 @@ public static class ServiceDiscoveryUtilities
         return Environment.GetEnvironmentVariable(
             $"services__{serviceName}__{endpointName}__{index}"
         );
+    }
+
+    public static string? GetServiceEndpoint(string serviceName, int index = 0)
+    {
+        return Environment.GetEnvironmentVariable(
+                $"services__{serviceName}__{Protocols.Https}__{index}"
+            )
+            ?? Environment.GetEnvironmentVariable(
+                $"services__{serviceName}__{Protocols.Http}__{index}"
+            );
     }
 }
