dc3786cd5a80e58612a3cc18e792963bc69ad5bc
Nhan Nguyen <nguyenxuannhan407@gmail.com>
2025-11-23 12:15:50 +0700

    feat: update package versions and refactor Quartz job configuration for improved clarity

--- src/Services/Scheduler/BookWorm.Scheduler/Extensions/Extensions.cs
+++ src/Services/Scheduler/BookWorm.Scheduler/Extensions/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using OpenTelemetry.Trace;
+using Quartz.AspNetCore;
 
 namespace BookWorm.Scheduler.Extensions;
 
@@ -15,29 +16,33 @@ internal static class Extensions
             q.SchedulerName = nameof(Scheduler);
             q.SchedulerId = $"{nameof(BookWorm)}-{nameof(Scheduler)}-{Environment.MachineName}";
             q.UseDefaultThreadPool(tp => tp.MaxConcurrency = Environment.ProcessorCount);
-            q.AddJobConfigurator<CleanUpSentEmailJob>(
-                builder.Configuration[$"{nameof(Quartz)}:{nameof(CleanUpSentEmailJob)}"]
-            );
-            q.AddJobConfigurator<ResendErrorEmailJob>(
-                builder.Configuration[$"{nameof(Quartz)}:{nameof(ResendErrorEmailJob)}"]
-            );
+            q.AddJobAndTrigger<CleanUpSentEmailJob>(builder.Configuration);
+            q.AddJobAndTrigger<ResendErrorEmailJob>(builder.Configuration);
         });
 
-        services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+        services.AddQuartzServer(q => q.WaitForJobsToComplete = true);
 
         services.AddOpenTelemetry().WithTracing(t => t.AddQuartzInstrumentation());
     }
 
-    private static void AddJobConfigurator<TJob>(
+    private static void AddJobAndTrigger<TJob>(
         this IServiceCollectionQuartzConfigurator quartz,
-        string? cronExpression,
+        IConfiguration config,
         Action<ITriggerConfigurator>? trigger = null
     )
         where TJob : IJob
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(cronExpression);
-
         var key = new JobKey(typeof(TJob).Name, nameof(Scheduler));
+
+        var configKey = $"{nameof(Quartz)}:{key.Name}";
+        var cronExpression = config[configKey];
+
+        if (string.IsNullOrWhiteSpace(cronExpression))
+        {
+            throw new ArgumentException(
+                $"Cron schedule not found in configuration for key: {configKey}"
+            );
+        }
 
         quartz.AddJob<TJob>(opts => opts.WithIdentity(key));
 
