6c703d87cfd4d51d70e61f4aef74ebcaed3995de
Nhan Nguyen <nguyenxuannhan407@gmail.com>
2026-04-28 00:55:06 +0700

    fix: change SpecificationBuilder from abstract to concrete class

--- src/BuildingBlocks/BookWorm.Chassis/Specification/Builders/SpecificationBuilder.cs
+++ src/BuildingBlocks/BookWorm.Chassis/Specification/Builders/SpecificationBuilder.cs
@@ -1,7 +1,6 @@
 namespace BookWorm.Chassis.Specification.Builders;
 
-internal abstract class SpecificationBuilder<T>(Specification<T> specification)
-    : ISpecificationBuilder<T>
+internal class SpecificationBuilder<T>(Specification<T> specification) : ISpecificationBuilder<T>
     where T : class
 {
     public Specification<T> Specification { get; } = specification;
